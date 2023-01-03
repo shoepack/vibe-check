d72d2775367d9ab48bab6be6400e59504dd5d275
shoepack <jtartley@yahoo.com>
2023-01-02 22:41:02 -0800

    initial commit, 1/2/23

--- src/main/java/com/vibecheck/VibecheckPlugin.java
+++ src/main/java/com/vibecheck/VibecheckPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.vibecheck;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Vibe Checker"
 )
-public class ExamplePlugin extends Plugin
+public class VibecheckPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private VibecheckConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	VibecheckConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(VibecheckConfig.class);
 	}
 }
